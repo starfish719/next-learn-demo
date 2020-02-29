be03674bcc2e4f974cc219307d259756ad763442
toda <starfish719@gmail.com>
2020-02-29 10:30:48 +0900

    Revert "finish lesson 1"
    This reverts commit b6c98a387b5a4b45c315f7a91374278824866f36.

--- 1-navigate-between-pages/pages/index.js
+++ 1-navigate-between-pages/pages/index.js
@@ -1,12 +1,7 @@
-import Link from 'next/link';
-
 export default function Index() {
   return (
     <div>
-      <Link href="/about">
-        <a title="About Page">About Page</a>
-      </Link>
       <p>Hello Next.js</p>
     </div>
-  );
+  )
 }
